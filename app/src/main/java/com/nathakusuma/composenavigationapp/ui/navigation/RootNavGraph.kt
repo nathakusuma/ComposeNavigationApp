098a42553e9a2355fd05397363392cc06abdf25b
nathakusuma <nathakusuma07@gmail.com>
2025-10-12 16:46:21 +0700

    2.1 - Buat RootNavGraph

---
+++ app/src/main/java/com/nathakusuma/composenavigationapp/ui/navigation/RootNavGraph.kt
@@ -0,0 +1,37 @@
+package com.nathakusuma.composenavigationapp.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.navigation
+import androidx.navigation.compose.composable
+
+@Composable
+fun RootNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.SPLASH
+    ) {
+        // 1) Splash sebagai start
+        composable(Routes.SPLASH) {
+            SplashScreen(
+                onFinished = {
+                    navController.navigate(Routes.MAIN_GRAPH) {
+                        popUpTo(Routes.SPLASH) { inclusive = true }
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+
+        // 2) Main graph (sementara)
+        navigation(
+            startDestination = Routes.HOME,
+            route = Routes.MAIN_GRAPH
+        ) {
+            composable(Routes.HOME) { PlaceholderHomeScreen() }
+        }
+    }
+}
