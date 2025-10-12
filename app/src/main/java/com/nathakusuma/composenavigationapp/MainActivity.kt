ee18ceed84b761945541e131d2b6af2c1f06af6f
nathakusuma <nathakusuma07@gmail.com>
2025-10-12 16:29:32 +0700

    1.5 - Mengatur MainActivity

--- app/src/main/java/com/nathakusuma/composenavigationapp/MainActivity.kt
+++ app/src/main/java/com/nathakusuma/composenavigationapp/MainActivity.kt
@@ -3,45 +3,20 @@ package com.nathakusuma.composenavigationapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.MaterialTheme
 import com.nathakusuma.composenavigationapp.ui.theme.ComposeNavigationAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ComposeNavigationAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Text(
+                    text = "Hello Compose Navigation",
+                    style = MaterialTheme.typography.headlineSmall
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeNavigationAppTheme {
-        Greeting("Android")
     }
 }
